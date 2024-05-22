52e3cafba4476cc5ea5707d8f812292d90246361
Yogesh Vashisth <writetokhair@gmail.com>
2024-05-22 15:27:22 +0530

    revising js

---
+++ JavaScript/script.js
@@ -0,0 +1,21 @@
+//understanding variables, dataTypes, operators Loops, conditionalStatemensts and Functions
+
+//variables
+var a = 12;
+var b = 13;
+
+a = 32;
+b = 33;
+
+// primitivces and references
+// {} [] () -refercee
+//other primitives
+
+//How to copy an references without the reference
+var b = [1, 2, 3];
+var d = [...b]; //now d is an individual array with the values of b (these three dost are known as spread operator)
+
+//Operaotrs + - * / %
+//Smart Opertors
+// 0 false undefined Nan null "" document.all
+//for eg false || 12 -> 12
