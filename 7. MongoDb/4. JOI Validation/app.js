3f4807394a8f40aaa26717797a3360f0eb9edcf6
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-17 17:26:34 +0530

    joi

--- 7. MongoDb/4. JOI Validation/app.js
+++ 7. MongoDb/4. JOI Validation/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-
+const { userModel, validateModel } = require("./models/user-model");
 const app = express();
 
-app.get("/", function (req, res) {});
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("chal rha hai");
+});
+
+app.post("/create", async function (req, res) {
+  const { name, username, age, contact, email } = req.body;
+  let error = validateModel({ name, username, age, contact, email });
+  if (error) return res.status(500).send(error.message);
+
+  res.send("everything worked");
+});
 
 app.listen(3000);
--- 7. MongoDb/4. JOI Validation/models/user-model.js
+++ 7. MongoDb/4. JOI Validation/models/user-model.js
@@ -1,3 +1,30 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
-mongoose.connect("");
+mongoose.connect(
+  "mongodb+srv://yogesh:yogesh@cluster0.7sqwppe.mongodb.net/joi"
+);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+  contact: Number,
+  email: String,
+});
+
+function validateModel(data) {
+  const userJoiSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().min(2).max(100).required(),
+    age: Joi.number().integer().min(1).max(120).required(),
+    contact: Joi.number().required(),
+    email: Joi.string().email().required(),
+  });
+  let { error } = userJoiSchema.validate(data);
+  return error;
+}
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = { userModel, validateModel };
