0f23c6da3a337b0f0fbd64def3becb2a40096deb
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-19 12:07:33 +0530

    done

--- 7. MongoDb/6. Aggregations/index.js
+++ 7. MongoDb/6. Aggregations/index.js
@@ -19,8 +19,38 @@ app.get("/", (res, res) => {
 //     {usme me se 25 umar se bade bale user nikalo}
 // ]);
 // });
-app.get("/test", async (res, res) => {
+app.get("/test", async (req, res) => {
   User.aggregate([{ $match: { age: 20 } }]);
+});
+
+// group iska matlab, jaise age wise users group kar dena
+
+app.get("/group", async (req, res) => {
+  let users = await User.aggregate([
+    {
+      $group: {
+        _id: "$age",
+        userdata: {
+          $push: "$$ROOT",
+          // yeh $$ROOT sara data laake dega :)
+        },
+      },
+    },
+  ]);
+});
+
+// kisi bhi collection mein agar koi data ek id hai to app usko actual data se fill kar sakte ho bilkul populate ki tarah
+app.get("/group", async (req, res) => {
+  let users = await Post.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localFeild: "author",
+        foriegnFeild: "_id",
+        as: "authordata",
+      },
+    },
+  ]);
 });
 
 app.listen(3000, () => {
